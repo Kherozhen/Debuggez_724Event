5540f800940ead2d39d4c29575733cf30dca8076
Khero <kherozhen@gmail.com>
2024-06-21 14:07:58 +0200

    edit key slide

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -1,7 +1,6 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useData } from "../../contexts/DataContext";
 import { getMonth } from "../../helpers/Date";
-
 import "./style.scss";
 
 const Slider = () => {
@@ -12,47 +11,46 @@ const Slider = () => {
     new Date(evtA.date) > new Date(evtB.date) ? -1 : 1
   );
 
-  // Utilisation de setInterval au lieu de setTimeout pour le changement automatique des slides
   useEffect(() => {
     const interval = setInterval(() => {
-      setIndex(prevIndex => (prevIndex + 1) % byDateDesc.length); // changement de diapo toutes les 5s
+      setIndex(prevIndex => (prevIndex + 1) % byDateDesc.length);
     }, 5000);
-    return () => clearInterval(interval); // Nettoyage de l'intervalle
+    return () => clearInterval(interval);
   }, [byDateDesc]);
 
   return (
     <div className="SlideCardList">
-      {byDateDesc?.map((event, idx) => (
-        <>
+      {byDateDesc?.map((focus, idx) => (
+        <React.Fragment key={focus.date}>
           <div
-            key={event.id}
             className={`SlideCard SlideCard--${
               index === idx ? "display" : "hide"
             }`}
           >
-            <img src={event.cover} alt="forum" />
+            <img src={focus.cover} alt="forum" />
             <div className="SlideCard__descriptionContainer">
               <div className="SlideCard__description">
-                <h3>{event.title}</h3>
-                <p>{event.description}</p>
-                <div>{getMonth(new Date(event.date))}</div>
+                <h3>{focus.title}</h3>
+                <p>{focus.description}</p>
+                <div>{getMonth(new Date(focus.date))}</div>
               </div>
             </div>
           </div>
+
           <div className="SlideCard__paginationContainer">
             <div className="SlideCard__pagination">
               {byDateDesc.map((_, radioIdx) => (
                 <input
-                  key={_.date} // clé unique et suit le slide grâce à la date
+                  key={_.date}
                   type="radio"
                   name="radio-button"
                   checked={index === radioIdx}
-                  readOnly // permet à la dot de suivre les images
+                  readOnly
                 />
               ))}
             </div>
           </div>
-        </>
+        </React.Fragment>
       ))}
     </div>
   );
